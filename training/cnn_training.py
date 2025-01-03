866069fbf08454237ccc45fb1008a80404d95f4f
LouisOporto <louisoporto042@ecs.fullerton.edu>
2025-01-03 00:24:08 -0800

    Custom training and testing module with multiple user inputted parameters for training and testing

--- training/cnn_training.py
+++ training/cnn_training.py
@@ -1,24 +1,23 @@
-print("Create env environment before proceeding in the trianing process.\n"
+print("Create env environment before proceeding in the training process.\n"
       "Run the following command in the terminal: python3 -m venv env\n"
       "Activate the environment by running: source env/bin/activate\n"
 
         "Install the required packages by running: pip install -r requirements.txt\n")
 
-import kagglehub
+import kagglehub # Uploading from kagglehub the EMNIST dataset from crawford
 
-import os
+import os # os and sys to chdir to the project working directory
 import sys
 
-import numpy as np # linear algebra
+import numpy as np # method to output images
 import struct
 from array import array
-from os.path  import join
 
 import random
 import matplotlib.pyplot as plt
 
 import tensorflow as tf
-from keras import layers, models, callbacks, datasets
+from keras import layers, models, callbacks
 from timeit import default_timer as timer
 
 from delay_callback import EpochDelayCallback
@@ -30,14 +29,13 @@ path = kagglehub.dataset_download('crawford/emnist')
 print("Path to the downloaded dataset: ", path)
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.insert(0, project_root)
-print("Project root: ", project_root)
-print("System root: ", sys.path)
 os.chdir(project_root)
+# print("Project root: ", project_root)
+# print("System root: ", sys.path)
 
 #
 # EMNIST Data Loader Class
 #
-
 class EmnistDataloader(object):
     def __init__(self, training_images_filepath, training_labels_filepath, test_images_filepath, test_labels_filepath):
         self.training_images_filepath = training_images_filepath
@@ -82,7 +80,6 @@ class EmnistDataloader(object):
         x_test, y_test = self.read_images_labels(self.test_images_filepath, self.test_labels_filepath)
         return (x_train, y_train),(x_test, y_test) 
     
-
 #
 # Set file paths based on added EMNIST Datasets
 #
@@ -116,41 +113,93 @@ def show_images(images, title_texts):
         index += 1
 
     plt.show()
+
 #
-# Load EMINST dataset
+# Show some random training and test images 
 #
-emnist_datalodaer = EmnistDataloader(training_images_filepath, training_labels_filepath, test_images_filepath, test_labels_filepath)
-(x_train, y_train), (x_test, y_test) = emnist_datalodaer.load_data()
+def sample_emnist():
+    images_2_show = []
+    titles_2_show = []
+    for i in range(0, 9):
+        r = random.randint(1, 60000)
+        images_2_show.append(x_train[r])
+        titles_2_show.append('training image [' + str(r) + '] = ' + str(y_train[r]))    
+
+    for i in range(0, 3):
+        r = random.randint(1, 10000)
+        images_2_show.append(x_test[r])        
+        titles_2_show.append('test image [' + str(r) + '] = ' + str(y_test[r]))    
+
+    show_images(images_2_show, titles_2_show)
+    ''
 
 
-# #
-# # Show some random training and test images 
-# #
+### LeNet with Keras and TensorFlow ###
+# Training module
+def train_model(model=None, epoch=100, sleep=30):
+    if not os.path.exists('training/emnist_model.keras'):
+        print("Creating new model")
+    else:
+        print("Loading from existing")
+        model = models.load_model('training/emnist_model.keras')
+        # model.load_weights('training/emnist_model.weights.h5')
+    
+    start = timer()
 
-# images_2_show = []
-# titles_2_show = []
-# for i in range(0, 9):
-#     r = random.randint(1, 60000)
-#     images_2_show.append(x_train[r])
-#     titles_2_show.append('training image [' + str(r) + '] = ' + str(y_train[r]))    
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy']) # Check what other metrics can be analayzed
+    
+    checkpoint_path = "training/emnist_model.weights.h5"
+    cp_callback = callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                            save_weights_only=True,
+                                            verbose=1)
 
-# for i in range(0, 3):
-#     r = random.randint(1, 10000)
-#     images_2_show.append(x_test[r])        
-#     titles_2_show.append('test image [' + str(r) + '] = ' + str(y_test[r]))    
+    early_stopping = callbacks.EarlyStopping(monitor='val_accuracy',
+                                            patience=10)
+    history = model.fit(x_train, y_train,
+                        epochs=epoch, # 100 epochs is 1 hour with RTX 4080
+                        validation_data=(x_test, y_test),
+                        callbacks=[early_stopping, cp_callback, EpochDelayCallback(delay_seconds=sleep)],
+                        verbose=1)
+    
 
-# show_images(images_2_show, titles_2_show)
+    print("Total Time consumed for 5 epochs -->", timer()-start)
 
+    model.save('training/emnist_model.keras')
 
-## LeNet with Keras and TensorFlow ##
+# Testing module
+def test_model(model=None, start_index=0, size=100):
+    if not os.path.exists('training/emnist_model.keras'):
+        print("Can't evaluate without saved model")
+    else:
+        print("Evaluating accuracy of training model")
+        model = models.load_model('training/emnist_model.keras')
 
+    x_rand_test = x_test[0:20]
+    y_rand_test = y_test[0:20]
+    print(x_rand_test)
+    print(y_rand_test)
+    
+    result = model.predict(x_rand_test, 20, 1, )
+    result = [np.argmax(ix) for ix in result]
+
+    print(result)
+
+    # Overall accuracy on test images
+    model.evaluate(x_test, y_test)
+
+emnist_dataloader = EmnistDataloader(training_images_filepath, training_labels_filepath, test_images_filepath, test_labels_filepath)
+(x_train, y_train), (x_test, y_test) = emnist_dataloader.load_data()
+# sample_emnist()
 
 # Check for GPU available
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
-print(tf.config.list_physical_devices())
+available_GPU = "Num GPUs Available: ", len(tf.config.list_physical_devices('GPU'))
+# print(available_GPU)
+info_GPU = tf.config.list_physical_devices()
+# print(info_GPU)
 
-# LeNet - 5 modifed for 62 outputs
-
+# LeNet - 5 modified for 62 outputs
 model = models.Sequential()
 model.add(layers.Input(shape=(28, 28, 1)))
 model.add(layers.Conv2D(filters=32, kernel_size=(5, 5), padding='same', activation='relu'))
@@ -163,42 +212,24 @@ model.add(layers.Dense(84, activation='relu'))
 model.add(layers.Dense(62, activation='softmax'))
 
 # model.build()
-
 # Show composition of model
 # model.summary()
 
-def train_model(model=None):
-    if not os.path.exists('training/emnist_model.keras'):
-        print("Creating new model")
-        model.compile(optimizer='adam',
-                    loss='sparse_categorical_crossentropy',
-                    metrics=['accuracy'])
+# Main Loop
+while (True):
+    print("Train or evaluate?")
+    user_input = input("(T) or (E):")
+
+    if user_input.upper() == 'T':
+        epoch = int(input("Epochs (50 epochs = ~30min): "))
+        sleep = int(input("Sleep Time Between Epochs(sec): "))
+        train_model(model, epoch, sleep)
+        break
+    elif user_input.upper() == 'E':
+        start_index = int(input("Starting index of test_images:"))
+        size = int(input("Size of input batch:"))
+        test_model(model, start_index, size)
+        break
     else:
-        print("Loading from existing")
-        model = models.load_model('training/emnist_model.keras')
-    
-    start = timer()
-
-    
-    checkpoint_path = "training/emnist_model.weights.h5"
-    cp_callback = callbacks.ModelCheckpoint(filepath=checkpoint_path,
-                                            save_weights_only=True,
-                                            verbose=1,
-                                            save_freq=1)
-
-    early_stopping = callbacks.EarlyStopping(monitor='val_accuracy',
-                                            patience=10)
-    history = model.fit(x_train, y_train,
-                        epochs=5,
-                        validation_data=(x_test, y_test),
-                        callbacks=[early_stopping, cp_callback, EpochDelayCallback(delay_seconds=10)],
-                        verbose=1)
-
-    
-    print("Total Time consumed for 5 epochs -->", timer()-start)
-    
-
-    model.save('training/emnist_model.keras')
-
-train_model(model)
+        print("Invalid input try another")
 
