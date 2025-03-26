4acd41e060531465104445a04e043419406a5575
LouisOporto <louisoprto042@csu.fullerton.edu>
2025-03-26 14:37:43 -0700

    Include API calls to upload and use CURL to upload image

--- notetify_project/recognition/urls.py
+++ notetify_project/recognition/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import HandwritingRecognitionView
+from .views import HandwritingRecognitionView, upload_image
 
 urlpatterns = [
-    path('recognize/', HandwritingRecognitionView.as_view(), name='recognize')
+    path('recognize/', HandwritingRecognitionView.as_view(), name='recognize'),
+    path('upload/', upload_image, name='upload_image'),
 ]
+
+#  curl -X POST -F "image=@src/NCR_samples/top-flash-sample-2.jpg" http://127.0.0.1:8000/api/recognize/ # Usage for CURL to website
