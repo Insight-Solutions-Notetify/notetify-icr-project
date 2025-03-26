021f61759795e7353071240658792691f40adf76
LouisOporto <louisoporto042@csu.fullerton.edu>
2025-03-25 23:58:36 -0700

    Inital location for logger and batchpipeline to reside

--- src/modules/batch_processing.py
+++ src/modules/batch_processing.py
@@ -13,7 +13,11 @@ os.chdir(project_root)
 
 from src.modules.logger import logger
 
-# TODO - Design
+# TODO - Given a set of images to run the recognition module, load the images and execute the process for each image one at a time.
+# Work orders will be sent as a request to work on "named files" when the request is given (parallel to other process)
+
 
 if __name__ == "__main__":
     logger.info("Testing batchpipeline")
+    # MOCK FUNCTIOn OF LOADING IMG (RANDOM IMG), then viewing image (RANDOM READY IMAGE)
+
