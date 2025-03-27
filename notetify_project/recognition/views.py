92ee6331a308905daa9764a19a0b1e23af106c17
LouisOporto <louisoporto042@csu.fullerton.edu>
2025-03-26 21:01:18 -0700

    HandwritingRecognitionView aside not main use of django

--- notetify_project/recognition/views.py
+++ notetify_project/recognition/views.py
@@ -18,38 +18,6 @@ from ml_modules.modules.segmentation import segmentate_image, segment_characters
 model = models.load_model("model/emnist_model.keras")
 model.load_weights("model/emnist_model_loss0.68.weights.h5")
 
-class HandwritingRecognitionView(views.APIView):
-    def post(self, request):
-        serializer = HandwritingImageSerializer(data=request.data)
-        if serializer.is_valid():
-            image = serializer.validated_data["image"]
-            image_path = default_storage.save(image.name, image)
-
-            uploaded_image = cv2.imread(f"media/{image_path}")
-        
-            # Send to preprocess and segmentation modules
-            preprocessed = preprocessImage(uploaded_image)
-            # segmented = segmentateImage(image)
-            # segmented = segment_characters(preprocessed) #TEMP while segementImage is being worked on
-
-            success, img_buffer = cv2.imencode(".jpg", preprocessed)
-            if not success:
-                raise Exception("Failed to encode image")
-            image_file = ContentFile(img_buffer.tobytes(), name='processed_image.jpg')
-
-            default_storage.save("processed-" + image.name, image_file)
-            predicted_text = "Predicted text..."
-            return Response({"text": predicted_text, "path": image_path}, status=status.HTTP_200_OK) 
-        
-            # segmented will be a list of lines, which is a list of words, which is a list of char images
-            # Need to deconstruct down to a list of char images by line to processed
-
-            predictions = model.predict(segmented)
-            predicted_text = decode_prediction(predictions)
-            return Response({"text": predicted_text}, status=status.HTTP_200_OK)
-        
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
 def upload_image(request):
     if request.method == 'POST' and request.FILES.get('image'):
         image = request.FILES["image"]
@@ -86,3 +54,35 @@ def decode_prediction(predictions):
             printed_guess[ix] = ' '
     
     return printed_guess
+
+class HandwritingRecognitionView(views.APIView):
+    def post(self, request):
+        serializer = HandwritingImageSerializer(data=request.data)
+        if serializer.is_valid():
+            image = serializer.validated_data["image"]
+            image_path = default_storage.save(image.name, image)
+
+            uploaded_image = cv2.imread(f"media/{image_path}")
+        
+            # Send to preprocess and segmentation modules
+            preprocessed = preprocessImage(uploaded_image)
+            # segmented = segmentateImage(image)
+            # segmented = segment_characters(preprocessed) #TEMP while segementImage is being worked on
+
+            success, img_buffer = cv2.imencode(".jpg", preprocessed)
+            if not success:
+                raise Exception("Failed to encode image")
+            image_file = ContentFile(img_buffer.tobytes(), name='processed_image.jpg')
+
+            default_storage.save("processed-" + image.name, image_file)
+            predicted_text = "Predicted text..."
+            return Response({"text": predicted_text, "path": image_path}, status=status.HTTP_200_OK) 
+        
+            # segmented will be a list of lines, which is a list of words, which is a list of char images
+            # Need to deconstruct down to a list of char images by line to processed
+
+            predictions = model.predict(segmented)
+            predicted_text = decode_prediction(predictions)
+            return Response({"text": predicted_text}, status=status.HTTP_200_OK)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
