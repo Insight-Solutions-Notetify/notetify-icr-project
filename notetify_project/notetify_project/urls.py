2a980f376c64733d2d7f3495c106565054374625
LouisOporto <louisoporto042@csu.fullerton.edu>
2025-04-02 23:22:13 -0700

    Add and include default to the project apps

--- notetify_project/notetify_project/urls.py
+++ notetify_project/notetify_project/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('recognition.urls')),
+    path('', include ('default.urls')),
 ]
 
 from django.conf import settings
