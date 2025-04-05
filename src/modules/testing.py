9949a50d232e151369869ca348a7fb48af3c2a33
LouisOporto <louisoporto042@csu.fullerton.edu>
2025-04-04 21:15:26 -0700

    Delete testing.py

--- src/modules/testing.py
+++
@@ -1,8 +0,0 @@
-""" This is where we will run tests for our project"""
-
-if __name__ == "__main__":
-    print("Running script to test all module functions")
-    print("Input image for translation")
-
-    image_input = input("Filename: ")
-    
