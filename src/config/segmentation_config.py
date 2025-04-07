e36270cdbf9fde697567f96e17c391ca0fb4d5e3
LouisOporto <louisoporto042@csu.fullerton.edu>
2025-04-06 17:23:35 -0700

    Separate config into each segmentation config

--- src/config/segmentation_config.py
+++ src/config/segmentation_config.py
@@ -5,13 +5,21 @@ class SegmentationConfig:
     MIN_WORD_GAP = 5
     MIN_LINE_GAP = 4
 
+    # Line Segmentation
+    HEIGHT_CHAR_BUFFER = 10
+    LINE_GAP_FACTOR = 0.5
+    TEXT_LINE_THRESHOLD = 0.05
+
+    # Word Segmentation
+    WIDTH_CHAR_BUFFER = 10
+    WORD_GAP_FACTOR = 1.8
+    
     # Character Segmentation
     MERGING_MIN_X = 3
     MERGING_MIN_Y = 10
 
     MINIMUM_SPLIT_WIDTH = 1.6 # Ignore splitting if average width goes below this value
     HEIGHT_INFLUENCE = 0.25
-    WIDTH_CHAR_BUFFER = 5
 
     # Post processing
     IMAGE_DIMS = (28, 28)
