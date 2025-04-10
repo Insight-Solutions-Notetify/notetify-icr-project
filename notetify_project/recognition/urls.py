250d1f82a555fb33c434c79acf02353182a3536e
LouisOporto <louisoprto042@csu.fullerton.edu>
2025-04-10 03:18:02 -0700

    New separation of functionality for uploading and viewing processed image instead of single page for viewing

--- notetify_project/recognition/urls.py
+++ notetify_project/recognition/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import UploadImageView, ImageStatusView, upload_page
+from .views import UploadImageView, ImageStatusView, upload_page, upload_image, result_view
 
 urlpatterns = [
+    # Separated and modularized methods (NEW)
+    path('only_upload/', upload_image, name='upload_image'),
+    path('result/<uuid:image_id>/', result_view, name='result_view'),
+
+    # Old methods
     path('upload/', upload_page, name='upload_page'),
     path('api/upload/', UploadImageView.as_view(), name='upload_image'),
     path('api/status/<uuid:task_id>/', ImageStatusView.as_view(), name='image_status'),
--- notetify_project/recognition/views.py
+++ notetify_project/recognition/views.py
@@ -1,10 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+from django.conf import settings
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from .models import HandwritingImage
 from .serializers import HandwritingImageSerializer
 from .tasks import process_upload_image
+
+
+def upload_image(request):
+    if request.method == 'POST' and request.FILES.get('image'):
+        image_file = request.FILES['image']
+        image_obj = HandwritingImage.objects.create(image=image_file)
+
+        task = process_upload_image.delay(image_obj.id)
+        image_obj.task_id = task.id
+        image_obj.save()
+
+        return render(request, 'upload_success.html', {'image_id': task.id})
+    return render(request, 'upload_file.html')
+
+def result_view(request, image_id):
+    try:
+        image = HandwritingImage.objects.get(task_id=image_id)
+        return render(request, 'result.html', {'image': image})
+    except HandwritingImage.DoesNotExist:
+        return Response({'error': 'Not found'}, status=404)
 
 def upload_page(request):
     return render(request, 'upload.html')
@@ -22,7 +45,6 @@ class UploadImageView(APIView):
     
 class ImageStatusView(APIView):
     def get(self, request, task_id):
-        print("test")
         try:
             obj = HandwritingImage.objects.get(task_id=task_id)
             return Response({
