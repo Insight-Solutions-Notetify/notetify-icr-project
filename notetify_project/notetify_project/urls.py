3ff3dfaff84310f44f9ba89f3b4adc1a7cad9b72
LouisOporto <louisoprto042@csu.fullerton.edu>
2025-04-09 23:26:29 -0700

    Update urls to match new views and redirect default to main

--- notetify_project/notetify_project/urls.py
+++ notetify_project/notetify_project/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('__reload__/', include('django_browser_reload.urls')),
-    path('api/', include('recognition.urls')),
+    path('', include('recognition.urls')),
     path('', include ('default.urls')),
 ]
 
