f68a9935b00353c0d53374d54c9f870fbdcdbacc
LouisOporto <louisoporto042@csu.fullerton.edu>
2025-04-18 00:35:41 -0700

    Simple linking to general pages

--- notetify_project/default/urls.py
+++ notetify_project/default/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import main
+from .views import main, about, contact
 
 urlpatterns = [
     path('', main, name='main'),
+    path('about-the-team', about, name='about'),
+    path('contact-us', contact, name='contact'),
 ]
