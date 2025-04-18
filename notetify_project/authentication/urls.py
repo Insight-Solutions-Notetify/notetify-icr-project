97c575b6abc7899907c637020902f07fa09059fc
LouisOporto <louisoporto042@csu.fullerton.edu>
2025-04-18 01:45:23 -0700

    New urls to register and login

---
+++ notetify_project/authentication/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import register_page, login_page
+
+urlpatterns = [
+    path('register', register_page, name='register'),
+    path('login', login_page, name='login'),
+]
--- notetify_project/notetify_project/urls.py
+++ notetify_project/notetify_project/urls.py
@@ -21,7 +21,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('__reload__/', include('django_browser_reload.urls')),
     path('', include('recognition.urls')),
-    path('', include ('default.urls')),
+    path('', include('default.urls')),
+    path('', include('authentication.urls')),
 ]
 
 from django.conf import settings
