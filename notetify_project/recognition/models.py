14802c62adede2bf00d7bdd73b84c71bb6e364f3
LouisOporto <louisoporto042@csu.fullerton.edu>
2025-04-18 02:41:07 -0700

    Allow uploading user to view uploaded file only and no one else (or else Forbidden tag)

--- notetify_project/recognition/models.py
+++ notetify_project/recognition/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class HandwritingImage(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="images")
     image = models.ImageField(upload_to="uploads/")
     processed_image = models.ImageField(upload_to='processed/', blank=True, null=True)
     recognized_text = models.TextField(blank=True)
--- notetify_project/recognition/views.py
+++ notetify_project/recognition/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile
 from django.conf import settings
+from django.http import HttpResponseForbidden
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
@@ -12,22 +13,30 @@ from .tasks import process_upload_image
 
 def upload_image(request):
     if request.method == 'POST' and request.FILES.get('image'):
-        image_file = request.FILES['image']
-        image_obj = HandwritingImage.objects.create(image=image_file)
-
-        task = process_upload_image.delay(image_obj.id)
-        image_obj.task_id = task.id
-        image_obj.save()
+        if request.user.is_authenticated:
+            image_file = request.FILES['image']
+            image_obj = HandwritingImage.objects.create(
+                user=request.user,
+                image=image_file
+            )
+            task = process_upload_image.delay(image_obj.id)
+            image_obj.task_id = task.id
+            image_obj.save()
 
         return render(request, 'upload_success.html', {'image_id': task.id})
     return render(request, 'upload_file.html')
 
 def result_view(request, image_id):
-    try:
-        image = HandwritingImage.objects.get(task_id=image_id)
-        return render(request, 'result.html', {'image': image})
-    except HandwritingImage.DoesNotExist:
-        return Response({'error': 'Not found'}, status=404)
+    image = HandwritingImage.objects.get(task_id=image_id)
+    image = get_object_or_404(HandwritingImage, task_id=image_id)
+
+    if image.user != request.user:
+        return HttpResponseForbidden("You do not have permission to view this image.")
+    
+    return render(request, 'result.html', {
+        'image': image,
+        'task_id': image.task_id,
+    })
 
 def upload_page(request):
     return render(request, 'upload.html')
