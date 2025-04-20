82d3e8d5c8d072be1f29c3bd0320c11e55ae24e1
LouisOporto <louisoprto042@csu.fullerton.edu>
2025-04-19 17:26:47 -0700

    Add private media page

--- notetify_project/notetify_project/settings.py
+++ notetify_project/notetify_project/settings.py
@@ -161,3 +161,6 @@ CELERY_BROKER_URL = 'redis://localhost:6379/0'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+PRIVATE_MEDIA_ROOT = os.path.join(BASE_DIR, 'private_media')
+PRIVATE_MEDIA_URL = '/private_media/'
+
