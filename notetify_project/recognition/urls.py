8808ee0039b563954738515cb2a24f5dcd2edd29
LouisOporto <louisoprto042@csu.fullerton.edu>
2025-04-20 03:13:22 -0700

    Limiting image load to 10 files and interval checks of image processing status

--- notetify_project/recognition/urls.py
+++ notetify_project/recognition/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, re_path
-from .views import upload_image, result_view, my_uploads, delete_image, reprocess_image
+from .views import upload_image, result_view, my_uploads, check_processing_status, delete_image, reprocess_image
 
 urlpatterns = [
     # Separated and modularized methods (NEW)
@@ -7,6 +7,7 @@ urlpatterns = [
     path('result/<uuid:image_id>/', result_view, name='result_view'),
 
     path('my_uploads/', my_uploads, name = 'my_uploads'),
+    path("api/check_status/", check_processing_status, name="check_processing_status"),
     path('image/<int:id>/delete/', delete_image, name='delete_image'),
     path('image/<int:id>/reprocess/', reprocess_image, name='reprocess_image'),
 ]
--- notetify_project/recognition/views.py
+++ notetify_project/recognition/views.py
@@ -1,24 +1,32 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.core.paginator import Paginator
-from django.http import HttpResponseForbidden
+from django.http import HttpResponseForbidden, JsonResponse
 from .models import HandwritingImage
 from .tasks import process_upload_image
 
 @login_required
 def upload_image(request):
-    if request.method == 'POST' and request.FILES.get('image'):
-        if request.user.is_authenticated:
-            image_file = request.FILES['image']
-            image_obj = HandwritingImage.objects.create(
-                user=request.user,
-                image=image_file
-            )
-            task = process_upload_image.delay(image_obj.id)
-            image_obj.task_id = task.id
-            image_obj.save()
+    if request.method == 'POST':
+        files = request.FILES.get('image')
+        current_count = HandwritingImage.objects.filter(user=request.user).count()
+        if current_count + 1 > 10:
+            messages.error(request, f"You can't upload any more image. Delete old files to upload more.")
+            return redirect('upload')
+        
+        image_file = request.FILES['image']
+        image_obj = HandwritingImage.objects.create(
+            user=request.user,
+            image=image_file
+        )
+        task = process_upload_image.delay(image_obj.id)
+        image_obj.task_id = task.id
+        image_obj.save()
 
-        return render(request, 'upload_success.html', {'image_id': task.id})
+        messages.success(request, f"Image uploaded successfully.")
+        return redirect('my_uploads')
+    
     return render(request, 'upload_file.html')
 
 @login_required
@@ -36,12 +44,13 @@ def result_view(request, image_id):
 @login_required
 def my_uploads(request):
     images = HandwritingImage.objects.filter(user=request.user).order_by('-uploaded_at')
+    
     paginator = Paginator(images, 5)
 
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
-    return render(request, 'my_uploads.html', {'page_obj': page_obj})
+    return render(request, 'my_uploads.html', {'page_obj': page_obj, 'count': images.count()})
 
 @login_required
 def delete_image(request, id):
@@ -56,6 +65,19 @@ def reprocess_image(request, id):
     if request.method == "POST":
         image.processed = False
         image.recognized_text = ""
-        image.task_id = process_upload_image.delay(image.task_id)
+        image.task_id = process_upload_image.delay(image.id)
         image.save()
     return redirect('my_uploads')
+
+@login_required
+def check_processing_status(request):
+    images = HandwritingImage.objects.filter(user=request.user)
+    status_data = {
+        str(img.id): {
+            "processed": img.processed,
+            "recognized_text": img.recognized_text,
+            "processed_image_url": img.processed_image.url if img.processed_image else None
+        }
+        for img in images
+    }
+    return JsonResponse(status_data)
