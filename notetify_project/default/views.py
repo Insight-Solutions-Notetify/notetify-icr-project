d5535ebbb975974c47c4d904c282dc47192a688f
LouisOporto <louisoporto042@csu.fullerton.edu>
2025-04-20 21:11:31 -0700

    Skeleton for simple contact page SMTP setup

--- notetify_project/default/views.py
+++ notetify_project/default/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+from django.http import HttpResponse
+from django.core.mail import EmailMessage
+from django.conf import settings
 
 # Create your views here.
 def main(request):
@@ -7,8 +11,36 @@ def main(request):
 def about(request):
     return render(request, 'about.html')
 
-def contact(request):
-    return render(request, 'contact.html')
-
 def settings(request):
     return render(request, 'temp-settings.html')
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            phone = form.cleaned_data['phone']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+
+            # # Send an email
+            # EmailMessage(
+            #     'Contact Form Submission from {}'.format(name),
+            #     message,
+            #     'notetify@gamil.com', # Send from
+            #     ['admin@gmail.com'], # Send to
+            #     [],
+            #     reply_to=[email] # Email from the form to get back to
+            # ).send()
+
+            return redirect('success')
+        else:
+            return render(request, 'contact.html', {'form': form})
+    else:
+        form = ContactForm()
+    return render(request, 'contact.html', {'form': form})
+
+
+def success(request):
+    return render(request, 'success.html', {'message': 'Thank you for contacting us!'})
